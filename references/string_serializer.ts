ff705bdb14d6c2af2d94bc3b1669c9b9f8f3a99a
MierenManz <christiaanmens@gmail.com>
2022-04-21 22:26:28 +0200

    change name

--- references/string_serializer.ts
+++ references/string_serializer.ts
@@ -4,7 +4,7 @@ import { encode as varintEncode } from "https://deno.land/x/varint@v2.0.0/varint
  * @param { string } data - deserialized string
  * @returns { Uint8Array } serialized string without magic bytes
  */
-export function serializeIntoV8String(data: string): Uint8Array {
+export function serializeJsString(data: string): Uint8Array {
   const [varintBytes, length] = varintEncode(data.length);
   const stringEncoded = new TextEncoder().encode(data);
 
