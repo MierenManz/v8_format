1178a56cd3c43fad440ece6098822b9e43c61f1e
MierenManz <christiaanmens@gmail.com>
2022-05-06 19:46:52 +0200

    feat: adds more float (de)serializer reference tests

--- tests/float_test.ts
+++ tests/float_test.ts
@@ -1,10 +1,105 @@
 import { deserializeV8Float, serializeJsFloat } from "../references/float.ts";
 import { DENO_CORE } from "./core.ts";
-import { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.138.0/testing/asserts.ts";
 
 Deno.test({
   name: "Deserialize Float",
-  fn: async function (t) {},
+  fn: async function (t) {
+    await t.step({
+      name: "Deserialize Positive Float",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(12.69)
+          .subarray(2);
+
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, 12.69);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize Positive Float",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(12.69)
+          .subarray(2);
+
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, 12.69);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize Positive Float",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(12.69)
+          .subarray(2);
+
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, 12.69);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Serialize Number.MAX_SAFE_INTEGER + 0.69",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(Number.MAX_SAFE_INTEGER + 0.69)
+          .subarray(2);
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, Number.MAX_SAFE_INTEGER + 0.69);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize Number.MIN_VALUE",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(Number.MIN_VALUE)
+          .subarray(2);
+
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, Number.MIN_VALUE);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize Number.EPSILON",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(Number.EPSILON)
+          .subarray(2);
+        const res = deserializeV8Float(input);
+
+        assertEquals(res, Number.EPSILON);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize non float",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize(10)
+          .subarray(2);
+        assertThrows(() => deserializeV8Float(input));
+      },
+    });
+  },
 });
 
 Deno.test({
@@ -72,6 +167,13 @@ Deno.test({
         const res = serializeJsFloat(Number.EPSILON);
 
         assertEquals(res, ref);
+      },
+    });
+
+    await t.step({
+      name: "Serialize non float",
+      fn: function () {
+        assertThrows(() => serializeJsFloat(10));
       },
     });
   },
