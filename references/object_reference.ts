7a376986616375a15c1175df7bf628a586b54af6
MierenManz <christiaanmens@gmail.com>
2022-05-06 23:14:38 +0200

    feat: adds object reference deserializer

--- references/object_reference.ts
+++ references/object_reference.ts
@@ -1,6 +1,22 @@
-import { encode as varintEncode } from "https://deno.land/x/varint@v2.0.0/varint.ts";
+import {
+  decode32 as varintDecode,
+  encode as varintEncode,
+} from "https://deno.land/x/varint@v2.0.0/varint.ts";
+import { consume } from "./util.ts";
 
 export function serializeReference(idx: number): Uint8Array {
   const varint = varintEncode(idx)[0];
   return new Uint8Array([0x5E, ...varint]);
+}
+
+export function deserializeReference(
+  data: Uint8Array,
+  // deno-lint-ignore ban-types
+  objRefs: {}[],
+  // deno-lint-ignore ban-types
+): {} {
+  if (data[0] !== 0x5E) throw new Error("Not a v8 object reference");
+  const [idx, bytesUsed] = varintDecode(data, 1);
+  consume(data, bytesUsed);
+  return objRefs[idx];
 }
