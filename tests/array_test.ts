286b347be93747c8138578edd5760e8d187daa60
MierenManz <christiaanmens@gmail.com>
2022-05-07 00:41:39 +0200

    feat: Add deserialize tests for array

--- tests/array_test.ts
+++ tests/array_test.ts
@@ -1,9 +1,214 @@
-import {
-  assertEquals,
-  assertThrows,
-} from "https://deno.land/std@0.136.0/testing/asserts.ts";
-import { serializeJsArray } from "../references/array.ts";
-import { DENO_CORE } from "./core.ts";
+import { assertEquals, assertThrows } from "./_deps.ts";
+import { deserializeV8Array, serializeJsArray } from "../references/array.ts";
+import { DENO_CORE } from "./_core.ts";
+
+Deno.test({
+  name: "Deserialize Dense Array",
+  fn: async function (t) {
+    await t.step({
+      name: "Deserialize empty array",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize([])
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, []);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize regular array",
+      fn: function () {
+        const data: unknown[] = [
+          "value",
+          12,
+          12.58,
+          true,
+          false,
+          null,
+          undefined,
+          12n,
+        ];
+        data.push(data);
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize associative array",
+      fn: function () {
+        // deno-lint-ignore no-explicit-any
+        const data = [] as any;
+        data["key"] = "value";
+        data["int"] = 12;
+        data["float"] = 12.58;
+        data["boolT"] = true;
+        data["boolF"] = false;
+        data["NULL"] = null;
+        data["undefined"] = undefined;
+        data["bigint"] = 12n;
+        data["ref"] = data;
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize mixed array",
+      fn: function () {
+        const data = [
+          "value",
+          12,
+          12.58,
+          true,
+          false,
+          null,
+          undefined,
+          12n,
+          // deno-lint-ignore no-explicit-any
+        ] as any;
+        data.push(data);
+
+        data["key"] = "value";
+        data["int"] = 12;
+        data["float"] = 12.58;
+        data["boolT"] = true;
+        data["boolF"] = false;
+        data["NULL"] = null;
+        data["undefined"] = undefined;
+        data["bigint"] = 12n;
+        data["ref"] = data;
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+  },
+});
+
+Deno.test({
+  name: "Deserialize Sparse Array",
+  fn: async function (t) {
+    await t.step({
+      name: "Deserialize object as array",
+      fn: function () {
+        const input = DENO_CORE
+          .serialize({})
+          .subarray(2);
+        assertThrows(() => deserializeV8Array(input));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize regular array",
+      fn: function () {
+        const data: unknown[] = [
+          "value",
+          12,
+          12.58,
+          true,
+          false,
+          null,
+          undefined,
+          ,
+          12n,
+        ];
+        data.push(data);
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize associative array",
+      fn: function () {
+        // deno-fmt-ignore
+        // deno-lint-ignore no-explicit-any
+        const data = [,,] as any;
+        data["key"] = "value";
+        data["int"] = 12;
+        data["float"] = 12.58;
+        data["boolT"] = true;
+        data["boolF"] = false;
+        data["NULL"] = null;
+        data["undefined"] = undefined;
+        data["bigint"] = 12n;
+        data["ref"] = data;
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+
+    await t.step({
+      name: "Deserialize mixed array",
+      fn: function () {
+        const data = [
+          "value",
+          12,
+          12.58,
+          true,
+          false,
+          null,
+          undefined,
+          ,
+          12n,
+          // deno-lint-ignore no-explicit-any
+        ] as any;
+        data.push(data);
+
+        data["key"] = "value";
+        data["int"] = 12;
+        data["float"] = 12.58;
+        data["boolT"] = true;
+        data["boolF"] = false;
+        data["NULL"] = null;
+        data["undefined"] = undefined;
+        data["bigint"] = 12n;
+        data["ref"] = data;
+
+        const input = DENO_CORE
+          .serialize(data)
+          .subarray(2);
+
+        const res = deserializeV8Array(input);
+        assertEquals(res, data);
+        assertEquals(input, new Uint8Array(input.length).fill(0));
+      },
+    });
+  },
+});
 
 Deno.test({
   name: "Serialize Dense Array",
