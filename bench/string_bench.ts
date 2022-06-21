cbe6178769920568ad1fedaba08c5bcaca972f93
MierenManz <christiaanmens@gmail.com>
2022-06-21 23:07:34 +0200

    add two byte string

--- bench/string_bench.ts
+++ bench/string_bench.ts
@@ -5,6 +5,7 @@ import {
 import { DENO_CORE } from "../tests/_core.ts";
 
 const INPUT = "Hello Beautiful World!";
+const INPUT2 = "Hello Parking meter 😃";
 
 Deno.bench({
   name: "Serialize String (v8)",
@@ -41,3 +42,40 @@ Deno.bench({
     deserializeV8String(buff);
   },
 });
+
+Deno.bench({
+  name: "Serialize Two-Byte String (v8)",
+  group: "Serialize 2b",
+  baseline: true,
+  fn: () => {
+    DENO_CORE.serialize(INPUT2);
+  },
+});
+
+Deno.bench({
+  name: "Serialize Two-Byte String (js)",
+  group: "Serialize 2b",
+  fn: () => {
+    serializeJsString(INPUT2);
+  },
+});
+
+Deno.bench({
+  name: "Deserialize Two-Byte String (v8)",
+  group: "Deserialize 2b",
+  baseline: true,
+  fn: () => {
+    const buff = DENO_CORE.serialize(INPUT2);
+    DENO_CORE.deserialize(buff);
+  },
+});
+
+Deno.bench({
+  name: "Deserialize Two-Byte String (js)",
+  group: "Deserialize 2b",
+  fn: () => {
+    const buff = DENO_CORE.serialize(INPUT2).subarray(2);
+    deserializeV8String(buff);
+  },
+});
+
